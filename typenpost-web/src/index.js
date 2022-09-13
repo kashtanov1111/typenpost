1172447f56ed09caefdc2b6db2e342f02c77c351
kashtanov1111 <1kashtanov1111@gmail.com>
2022-09-13 21:40:13 +0200

    ch

--- typenpost-web/src/index.js
+++ typenpost-web/src/index.js
@@ -14,8 +14,8 @@ import { Authorization } from './components/Authorization';
 
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8000/graphql/',
-  // uri: 'https://www.typenpost.com/graphql/',
+  // uri: 'http://localhost:8000/graphql/',
+  uri: 'https://www.typenpost.com/graphql/',
   credentials: 'include',
   cache: new InMemoryCache(),
 })
