8731e9d052ab80ee15b953f9b894aa714299bac9
kashtanov1111 <1kashtanov1111@gmail.com>
2024-07-26 11:09:13 +0200

    changes

--- typenpost-frontend/src/index.js
+++ typenpost-frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 import reportWebVitals from './reportWebVitals';
-import { 
-  ApolloClient, 
-  InMemoryCache, 
+import {
+  ApolloClient,
+  InMemoryCache,
   ApolloProvider,
 } from '@apollo/client'
 import { BrowserRouter } from 'react-router-dom';
@@ -13,50 +13,51 @@ import { relayStylePagination } from '@apollo/client/utilities';
 
 const client = new ApolloClient({
   // uri: 'http://localhost:8000/graphql/',
-  uri: 'https://www.typenpost.com/graphql/',
+  // uri: 'https://www.typenpost.com/graphql/',
+  uri: 'https://typenpost1.herokuapp.com/graphql/',
   credentials: 'include', // remove in production
-  
+
   cache: new InMemoryCache({
     typePolicies: {
       UserProfileNode: {
-          fields: {
-            followers: relayStylePagination(),
-            following: relayStylePagination(),
-          }
+        fields: {
+          followers: relayStylePagination(),
+          following: relayStylePagination(),
+        }
       },
       UserNode: {
-          fields: {
-            posts: relayStylePagination(),
-          }
+        fields: {
+          posts: relayStylePagination(),
+        }
       },
       PostNode: {
-          fields: {
-            comments: relayStylePagination(),
-            likes: relayStylePagination()
-          }
+        fields: {
+          comments: relayStylePagination(),
+          likes: relayStylePagination()
+        }
       },
       CommentNode: {
-          fields: {
-            replies: relayStylePagination(),
-            likes: relayStylePagination(),
-          }
+        fields: {
+          replies: relayStylePagination(),
+          likes: relayStylePagination(),
+        }
       },
       Query: {
-          fields: {
-            feed: relayStylePagination(),
-          }
+        fields: {
+          feed: relayStylePagination(),
+        }
       },
     },
   }),
-  
+
 })
-  
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <ApolloProvider client={client}>
-      <App client={client}/>
+      <App client={client} />
     </ApolloProvider>
   </BrowserRouter>
 );
